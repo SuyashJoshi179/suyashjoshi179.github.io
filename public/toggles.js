bcb73fb5d7f510b837c1008b1f06244d91859bab
SuyashJoshi179 <suyash.joshi179@gmail.com>
2021-08-29 17:56:11 +0530

    Home page

---
+++ public/toggles.js
@@ -0,0 +1,16 @@
+import { createTheme } from '@material-ui/core';
+
+export const urls = {
+    resume: 'https://drive.google.com/file/d/19SDg_eb-noc95C7uZ_hLTPd9UYMVrP7U/view?usp=sharing',
+    github: 'https://github.com/SuyashJoshi179/',
+    linkedin: 'https://www.linkedin.com/in/suyash-joshi/',
+    email: 'mailto:suyash.joshi179@gmail.com',
+};
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#007FFF',
+        },
+    },
+});
