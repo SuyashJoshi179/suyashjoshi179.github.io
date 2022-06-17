a24c9be53d8ead9e646886df3ce93e66bd4c5456
SuyashJoshi179 <suyash.joshi179@gmail.com>
2022-06-17 22:38:59 +0530

    Skills Page- Web Developement Section

--- pages/skills/index.js
+++ pages/skills/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, Stack, ThemeProvider, Typography } from '@material-ui/core';
+import { Box, Card, CardContent, Divider, IconButton, Stack, ThemeProvider, Tooltip, Typography } from '@material-ui/core';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import CloseIcon from '@mui/icons-material/Close';
 import FunctionsIcon from '@mui/icons-material/Functions';
@@ -10,12 +10,12 @@ import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import Head from 'next/head';
-import Script from 'next/script';
 import Image from 'next/image';
-import { theme, urls } from '../../public/toggles';
+import Script from 'next/script';
+import data from "../../public/data.json";
+import { theme } from '../../public/toggles';
 import styles from '../../styles/Skills.module.css';
 import VerticalCarousel from './VertCarousal';
-import data from "../../public/data.json";
 
 export default function Home() {
     const actions = [
@@ -70,6 +70,51 @@ export default function Home() {
                         <VerticalCarousel data={rotateArray(data.slides, 16)} dir="next" dur={0} />
                         <VerticalCarousel data={rotateArray(data.slides, 24).reverse()} dir="prev" dur={0} />
                         <VerticalCarousel data={rotateArray(data.slides, 32)} dir="next" dur={0} />
+                    </Stack>
+                    <Stack className={styles.cards}>
+                        <Divider textAlign="left" sx={{ marginTop: "5vh", fontSize: "1.7rem" }} >Web Developement</Divider>
+                        <Card className={styles.card}>
+                            <CardContent sx={{ order: 1, padding: 0 }} >
+                                <Script src="/lottie-player.js" strategy='beforeInteractive' />
+                                <lottie-player src="/frontend.json" background="transparent" speed="1" loop autoplay></lottie-player>
+                            </CardContent>
+                            <CardContent className={styles.cardcont} sx={{ order: 2 }} >
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Frontend Developement
+                                </Typography>
+                                <Typography gutterBottom variant="body2" color="text.secondary">
+                                    I build complex dynamic websites which emphasize efficiency, responsiveness, interactivity and SEO.
+                                </Typography>
+                                {data.slides.filter(i => i.domain.includes("frontend")).map(i => (
+                                    <Tooltip key={i.name} title={i.name}>
+                                        <IconButton sx={{ border: `1px solid ${i.color}`, padding: "10px", margin: "5px" }} aria-label={i.name} >
+                                            <Image src={i.url} alt={i.name} width="30px" height="30px" loading="eager" unoptimized />
+                                        </IconButton>
+                                    </Tooltip>
+                                ))}
+                            </CardContent>
+                        </Card>
+                        <Card className={styles.card}>
+                            <CardContent sx={{ order: 2, padding: 0, width: "50%", height: "50%" }} >
+                                <Script src="/lottie-player.js" strategy='beforeInteractive' />
+                                <lottie-player src="/cloud.json" background="transparent" speed="1" loop autoplay></lottie-player>
+                            </CardContent>
+                            <CardContent className={styles.cardcont} sx={{ order: 1 }} >
+                                <Typography gutterBottom variant="h5" component="div">
+                                    Backend Developement
+                                </Typography>
+                                <Typography gutterBottom variant="body2" color="text.secondary">
+                                    I develope robust, secure, scalable and highly functional backends that adhere to the REST architectural constraints.
+                                </Typography>
+                                {data.slides.filter(i => i.domain.includes("backend") || i.domain.includes("cloud") || i.domain.includes("database")).map(i => (
+                                    <Tooltip key={i.name} title={i.name}>
+                                        <IconButton sx={{ border: `1px solid ${i.color}`, padding: "10px", margin: "5px" }} aria-label={i.name} >
+                                            <Image src={i.url} alt={i.name} width="30px" height="30px" loading="eager" unoptimized />
+                                        </IconButton>
+                                    </Tooltip>
+                                ))}
+                            </CardContent>
+                        </Card>
                     </Stack>
                 </main>
 
